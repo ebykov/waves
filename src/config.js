0273fc6722fd4780e92184324c007b42abde5a09
evgeny <bykovzhukov@yandex.ru>
2019-05-14 13:15:09 +0300

    fixes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   name: 'Waves', // уникальное имя спецпроекта. Оно же — название главного класса. Используется на странице, куда интегрируется спецпроект
-  analyticsCategory: 'Waves',
+  analyticsCategory: 'Waves Web 3.0',
   sendPageView: false, // отключаем, если спецпроект не на отдельной странице
   listenedEvents: ['click'], // слушаем события (click, input, change, etc.). Обычно нужен только click
 };
